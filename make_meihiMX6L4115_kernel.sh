4ad1b7655b4e02e7893977b69180e7d76d2ef639
hehaibo <hehaibo2003@126.com>
2016-08-06 13:12:13 +0800

    添加meihuan公司的dts设备树文件，启lvds0启动优先方式显示

--- make_meihiMX6L4115_kernel.sh
+++ make_meihiMX6L4115_kernel.sh
@@ -2,10 +2,10 @@ cd  kernel-imx6-4.1.15/
 make distclean
 make mh_imx6_v7_defconfig
 make -j8 zImage
-make imx6q-sabresd.dtb
-make imx6dl-sabresd.dtb
+make mh-imx6q-sabresd.dtb
+make mh-imx6dl-sabresd.dtb
 
 # Example:
 # mv arch/arm/boot/zImage /home/harry/TftpRoot/zImage-mh-imx6-6q
-# mv arch/arm/boot/dts/imx6q-sabresd.dtb  /home/harry/TftpRoot/zImage-mh-imx6-6q.dtb
+# mv arch/arm/boot/dts/mh-imx6q-sabresd.dtb  /home/harry/TftpRoot/zImage-mh-imx6-6q.dtb
 # mv arch/arm/boot/dts/mh-imx6dl-sabresd.dtb  /home/harry/TftpRoot/mhimx6-6s.dtb
